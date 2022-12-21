192c86c6a534a546720a7b00519e007114241813
Jeff Derbes <derbes.jeffrey@gmail.com>
2022-12-20 22:08:44 -0600

    copy updates

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Socialz
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
